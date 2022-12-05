ca62b5d2101cc6014b544ff187306ed50e1d7dde
Rushikesh Adhav <adhavrushikesh6@gmail.com>
2022-12-05 19:44:52 +0530

    .env update

--- server/index.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`App is running at http://localhost:${PORT}`);
-});
